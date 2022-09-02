423bb9a8c80cd8e92406339698288f2e3f04a94d
Kanazawa <naoaki65@gmail.com>
2022-09-02 22:13:40 +0900

    Fix manequine.py prevent relaying init-pose

--- scripts/pr2/mannequine.py
+++ scripts/pr2/mannequine.py
@@ -73,6 +73,7 @@ class Mannequin(object):
         self.enable_mirror = mirror
         self.use_home_position = use_home_position
 
+        robot.angle_vector(ri.angle_vector())
         time.sleep(3)
         self.reset_mannequin()
 
